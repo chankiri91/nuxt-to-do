172c6fd2aceb710f01233de2e4d2b443b300b7f3
Kiri-91 <>
2021-08-17 19:41:38 +0900

    新規項目の追加機能

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,19 @@
   <div id="app">
     <h1>Nuxt To Do App</h1>
     <div>
+      <label for="title">タイトル：</label>
+      <input type="text" v-model="title" placeholder="タイトル">
+    </div>
+    <div>
+      <label for="body">内容：</label>
+      <input type="text" v-model="body" placeholder="内容">
+    </div>
+    <div>
+      <input type="submit" value="追加" @click="add">
+    </div>
+    <div>
       <ul v-for="(list, i) in lists" v-bind:key="i">
-        <li>「{{list.title}}」, 詳細:{{list.body}}<button @click="clear">削除</button></li>
+        <li>「{{list.title}}」, 詳細:{{list.body}}<button @click="clear(i)">削除</button></li>
       </ul>
     </div>
   </div>
@@ -20,6 +31,13 @@ export default {
   methods: {
     clear: function(i) {
       this.lists.splice(i,1)
+    },
+    add: function() {
+      if(this.title === '' || this.body === '')return
+
+      this.lists.push({title: this.title, body: this.body})
+      this.title = ''
+      this.body = ''
     }
   }
 }
