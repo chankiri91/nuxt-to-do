46e72ac7828f64b238c5f6f1932f492b5971ef97
Kiri-91 <>
2021-08-17 17:53:20 +0900

    todoのタイトルと内容を表示

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,21 @@
 <template>
-  <div>
-    <h1>Hello Nuxt</h1>
+  <div id="app">
+    <h1>Nuxt To Do App</h1>
+    <div>
+      <ul v-for="(list, i) in lists" v-bind:key="i">
+        <li>「{{list.title}}」, 詳細:{{list.body}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data: () => ({
+    lists: [
+      {title: '買い物', body: '牛乳を買う'},
+      {title: '宿題', body: '数学12~24ページ'}
+    ]
+  })
 }
 </script>
