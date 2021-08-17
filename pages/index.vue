b352991ed8507ea42de2c2dee135b7c0881f05d8
Kiri-91 <>
2021-08-17 18:11:20 +0900

    削除ボタンの追加

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <h1>Nuxt To Do App</h1>
     <div>
       <ul v-for="(list, i) in lists" v-bind:key="i">
-        <li>「{{list.title}}」, 詳細:{{list.body}}</li>
+        <li>「{{list.title}}」, 詳細:{{list.body}}<button @click="clear">削除</button></li>
       </ul>
     </div>
   </div>
@@ -16,6 +16,11 @@ export default {
       {title: '買い物', body: '牛乳を買う'},
       {title: '宿題', body: '数学12~24ページ'}
     ]
-  })
+  }),
+  methods: {
+    clear: function(i) {
+      this.lists.splice(i,1)
+    }
+  }
 }
 </script>
